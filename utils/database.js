b94f4f8816b19aa04b1a4c834e053403d52c4e45
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2023-12-24 21:14:00 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -21,3 +21,23 @@ export const connectToDB=async ()=>{
         console.log(error);
     }
 }
+
+// export const connectToDB=async ()=>{
+//     mongoose.set('strictQuery',true);
+
+//     if(isConnected){
+//         console.log('MongoDB is already connected');
+//         return;
+//     }
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI,{
+//             dbName:'share_prompt',
+//             useNewURLparser:true,
+//             useUnifiedTopology:true
+//         })
+//         isConnected=true;
+//         console.log('MongoDB connected');
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
